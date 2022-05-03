507642e24f9561a030ff988708e59f4e4afe9679
Jesse Seeligsohn <jesseseeligsohn@gmail.com>
2022-05-03 18:19:21 -0400

    Changed the input function to parse a number (int) and a letter (unit). Created the logic for the scaling of sleepTime based on the seconds

--- sleep++.c
+++ sleep++.c
@@ -1,15 +1,28 @@
 #include "stdio.h"
 #include <time.h>
-
 #include <unistd.h>
 
-int input() {
+struct inp {
     int number;
-    //Checks to see if scanf succesfully read into number.
-    if (scanf("%ia", &number) != 1) {
-        printf("Please enter an integer");
+    char letter;
+};
+struct inp input() {
+    struct inp length;
+    char inp[11];
+    int test1;
+    char test2;
+    // fgets the size of the largest integer + a trailing character for unit size
+    if (fgets(inp,11,stdin) != NULL) {
+        //Reparse input into variables
+        int ct = sscanf(inp, "%i %c", &length.number, &length.letter);
+        //Check the number of succesful input assignments and process based on number of inputs
+        if (ct == 0) {
+            printf("Please enter an integer\n");
+        } else if (ct == 1) {
+            length.letter = 0;
+        }
     }
-    return number;
+    return length;
 }
 
 void printProgress (int length, int fill) {
@@ -22,19 +35,31 @@ void printProgress (int length, int fill) {
 }
 
 int main() {
-    int sleepTime = input();
-
+    struct inp ln = input();
+    int sleepTime;
+    switch (ln.letter){
+        case 'm':
+            sleepTime = ln.number * 60;
+            break;
+        case 'h':
+            sleepTime = ln.number * 3600;
+            break;
+        default:
+            sleepTime = ln.number;
+            break;
+    }
     time_t start = time(0);
-    int segments = 10;
+    int segments = 50;
     double interval = ((double )sleepTime / segments) * 10e5;
     double elapsed = 0;
     
     int count = 0;
     printf("\e[?25l");
     while (elapsed < sleepTime){
-        printProgress(segments , count);
-
-        fflush(stdout);
+        // if (count != )
+        // printProgress(segments , count);
+        // fflush(stdout);
+        printf("%c\n", ln.letter);
         usleep(interval);
         count++;
     
@@ -43,7 +68,7 @@ int main() {
 
     printProgress(segments,segments);
 
-    printf("\e[?25l");
+    printf("\e[?25h \n");
     return 0;
 
 
