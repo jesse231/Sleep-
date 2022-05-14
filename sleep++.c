e5181881548b786d561e40bb863734ab65e4c224
Jesse Seeligsohn <jesseseeligsohn@gmail.com>
2022-05-14 11:05:43 -0400

    Adds time display via the timeDisplay function.

--- sleep++.c
+++ sleep++.c
@@ -7,36 +7,59 @@
 int parse(char* inp) {
     int time;
     char letter;
-    //parse input into variables
+    // Parse input into variables (lets the letter be optional)
     int ct = sscanf(inp, "%i %c", &time, &letter);
 
-    //Check the number of succesful input assignments and process based on number of inputs
     if (ct == 0) {
         printf("Please enter an integer\n");
         return -1;
+    // Check if the letter was added or not
     } else if (ct == 1) {
         return time;
     }
 
-    //Scale time based on the unit ('m': minutes, 'h' hours) 
+    // Scale time based on the unit ('m': minutes, 'h' hours) 
     if (letter == 'm'){
         return time * 60;
     } else if (letter == 'h') {
         return time * 3600;
     }
 
-    //Otherwise print message and return failure
     printf("Please enter a valid unit (m or h)");
     return -1;
 }
 
 
-void printProgress (int length, int fill, int time) {
+void printProgress (int length, int fill, char* time) {
     printf("\r[");
     for (int i = 0; i < fill; ++i){
         printf("█");
     }
-    printf("%*c %d", length - fill,  ']', time);
+    printf("%*c %s", length - fill,  ']', time);
+
+}
+
+// Breaks seconds into a string with hours, minutes and seconds.
+void timeDisplay (int sec, char* res, int buffSize) {
+    char hours[50] = "";
+    char minutes[50] = "";
+    char seconds[50] = "";
+    int hr, min, s;
+
+    if ((hr = sec / (60 * 60))) {
+        snprintf(hours, sizeof(hours), "%ih ", hr);
+    }
+
+    if ((min = (sec - hr * (60 * 60)) / 60)) {
+        snprintf(minutes, sizeof(minutes), "%im ", min);
+    }
+
+    s = sec - hr * (60 * 60) - min * 60;
+    snprintf(seconds, sizeof(seconds), "%is", sec);
+
+    snprintf(res, buffSize, "%s%s%s", hours, minutes, seconds);
+
+
 
 }
 
@@ -67,7 +90,9 @@ int main(int argc, char** argv) {
 
     while (elapsed < sleepTime){
         if (interval * (double) count < elapsed) {
-            printProgress(segments, count, elapsed);
+            char time[50];
+            timeDisplay(elapsed, time, sizeof(time));
+            printProgress(segments, count, time);
             fflush(stdout);
             count++;
         }
@@ -75,7 +100,7 @@ int main(int argc, char** argv) {
         elapsed = (double) (clock() - start) / CLOCKS_PER_SEC;
     }
 
-    printProgress(segments,segments, (int) elapsed);
+    printProgress(segments ,segments ,"Done");
 
     printf("\e[?25h \n");
     return 0;
